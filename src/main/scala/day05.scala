d3a0f544ec703bbe253c1766fcfeed497cfabb3c
Yao-Chin Wu <yaochin.wu@gmail.com>
2016-01-06 18:38:58 -0800

    Day05

---
+++ src/main/scala/day05.scala
@@ -0,0 +1,24 @@
+import scala.io.StdIn
+
+object day05 {
+  def main(args: Array[String]) {
+    val t = StdIn.readLine().toInt
+
+    for ( i <- 1 to t ) {
+      val inputs = StdIn.readLine().split(' ')
+      val a = inputs(0).toInt
+      val b = inputs(1).toInt
+      val n = inputs(2).toInt
+
+      var result = a
+      for (j <- 0 until n) {
+        result = result + scala.math.pow(2, j).toInt * b
+        print(result + " ")
+      }
+
+      print("\n")
+    }
+
+  }
+
+}
