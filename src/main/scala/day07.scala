a154b6a39089aafc245a3c09464b607b4e46d250
Yao-Chin Wu <yaochin.wu@gmail.com>
2016-01-08 19:05:44 -0800

    Day07 & day08

---
+++ src/main/scala/day07.scala
@@ -0,0 +1,18 @@
+object day07 {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    val n = sc.nextInt()
+    val arr = new Array[Int](n)
+    for (arr_i <- 0 to n-1) {
+      arr(arr_i) = sc.nextInt()
+    }
+
+    for (i <- 1 to n) {
+      print(arr(n - i) + " ")
+    }
+
+
+  }
+
+}
---
+++ src/main/scala/day08.scala
@@ -0,0 +1,26 @@
+import scala.io.StdIn
+
+object day08 {
+
+  def main(args: Array[String]): Unit = {
+
+    val n = StdIn.readLine().toInt
+    var phoneBook = Map[String, String]()
+    for (i <- 1 to n) {
+      val name = StdIn.readLine()
+      val number = StdIn.readLine()
+      phoneBook = phoneBook + (name -> number)
+    }
+
+    Iterator.continually(StdIn.readLine())
+      .takeWhile(line => line != null && line.nonEmpty)
+      .foreach { input =>
+        val number = phoneBook.get(input)
+        number match {
+          case Some(value) => println(s"$input=$value")
+          case None => println("Not found")
+        }
+      }
+  }
+
+}
